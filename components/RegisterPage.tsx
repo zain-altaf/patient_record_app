e791d08d8a31fb5edd1381d137239f3ba3d1ad13
Zain Altaf <zaltaf@uwaterloo.ca>
2025-02-24 00:56:58 -0500

    made the sign in and register forms work and displaying the resources now

--- components/RegisterPage.tsx
+++ components/RegisterPage.tsx
@@ -1,24 +1,21 @@
-// 'use client';
+"use client"
 
-// import { RegisterForm } from '@medplum/react';
-// import { useRouter } from 'next/navigation';
-// import { Container, Title } from '@mantine/core';
+import { RegisterForm } from "@medplum/react"
+import { UserCircle } from "lucide-react"
 
-// const RegisterPage = () => {
-//   const router = useRouter();
+const googleClientId = "921088377005-3j1sa10vr6hj86jgmdfh2l53v3mp7lfi.apps.googleusercontent.com"
 
-//   return (
-//     <Container size="sm" mt="xl">
-//       <Title order={1} align="center" mb="xl">
-//         Register
-//       </Title>
-//       <RegisterForm
-//         type="signup"
-//         onSuccess={() => router.push('/')}
-//         googleClientId={googleClientId}
-//       />
-//     </Container>
-//   );
-// }
-
-// export default RegisterPage;
+export function RegisterPage() {
+  return (
+    <div className="bg-white rounded-lg shadow-lg p-8">
+      <RegisterForm googleClientId={googleClientId} type={"patient"} onSuccess={function (): void {
+              throw new Error("Function not implemented.")
+          } }>
+        <div className="text-center mb-6">
+          <UserCircle className="w-16 h-16 text-blue-500 mx-auto mb-2" />
+          <h2 className="text-2xl font-semibold text-gray-800">Register</h2>
+        </div>
+      </RegisterForm>
+    </div>
+  )
+}
--- components/SignInPage.tsx
+++ components/SignInPage.tsx
@@ -1,26 +1,26 @@
-'use client';
+"use client"
 
-import { BackgroundImage, Box, SimpleGrid } from '@mantine/core';
-import { SignInForm } from '@medplum/react';
-import { useRouter } from 'next/navigation';
-import { MEDPLUM_GOOGLE_CLIENT_ID, MEDPLUM_PROJECT_ID } from '../app/config';
+import { SignInForm } from "@medplum/react"
+import { UserCircle } from "lucide-react"
+import { useRouter } from "next/navigation"
+import { RegisterPage } from "./RegisterPage"
 
-export function SignInPage(): JSX.Element {
-  const router = useRouter();
+const googleClientId = "921088377005-3j1sa10vr6hj86jgmdfh2l53v3mp7lfi.apps.googleusercontent.com"
+
+export function SignInPage() {
+  const router = useRouter()
 
   return (
-    <SimpleGrid cols={2}>
-      <Box pt={100} pb={200}>
-        <SignInForm
-          projectId={MEDPLUM_PROJECT_ID}
-          googleClientId={MEDPLUM_GOOGLE_CLIENT_ID}
-          onSuccess={() => router.push('/')}
-          onRegister={() => router.push('/register')}
-        >
-          <h2>Sign in to Foo Medical</h2>
-        </SignInForm>
-      </Box>
-      <BackgroundImage src="https://images.unsplash.com/photo-1556761175-4b46a572b786?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=1567&amp;q=80" />
-    </SimpleGrid>
-  );
+    <div className="bg-white rounded-lg shadow-lg p-8">
+      <SignInForm 
+        googleClientId={googleClientId}
+        onRegister={() => router.push('/register')}
+      >
+        <div className="text-center mb-6">
+          <UserCircle className="w-16 h-16 text-blue-500 mx-auto mb-2" />
+          <h2 className="text-2xl font-semibold text-gray-800">Sign in</h2>
+        </div>
+      </SignInForm>
+    </div>
+  )
 }
