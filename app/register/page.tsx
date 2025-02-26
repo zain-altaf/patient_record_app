c5dd98c466eddb02929807534a9ea7a01c99d63d
Zain Altaf <zaltaf@uwaterloo.ca>
2025-02-26 15:58:05 -0500

    added configurations for env variables

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -5,10 +5,10 @@ import { RegisterForm, useMedplumProfile } from '@medplum/react';
 import { UserCircle } from 'lucide-react';
 import { useRouter } from 'next/navigation';
 
-// TODO: change in prod since it's configured to run on localhost:3000
-const googleClientId = '921088377005-3j1sa10vr6hj86jgmdfh2l53v3mp7lfi.apps.googleusercontent.com';
-const MEDPLUM_PROJECT_ID = '9602358d-eeb0-4de8-bccf-e2438b5c9162';
-export const MEDPLUM_RECAPTCHA_SITE_KEY = '6LfFd_8gAAAAAOCVrZQ_aF2CN5b7s91NEYIu5GxL';
+// Use environment variables for sensitive data
+const googleClientId = process.env.GOOGLE_CLIENT_ID;
+const MEDPLUM_PROJECT_ID = process.env.MEDPLUM_PROJECT_ID;
+export const MEDPLUM_RECAPTCHA_SITE_KEY = process.env.MEDPLUM_RECAPTCHA_SITE_KEY;
 
 export default function RegisterPage() {
   const router = useRouter();
