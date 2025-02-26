4675e7e4868809dc0491db86c00c821a87a069cf
Zain Altaf <zaltaf@uwaterloo.ca>
2025-02-26 16:29:50 -0500

    added config stuff

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  env: {
+    NEXT_PUBLIC_GOOGLE_CLIENT_ID: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
+    NEXT_PUBLIC_MEDPLUM_GOOGLE_CLIENT_ID: process.env.NEXT_PUBLIC_MEDPLUM_GOOGLE_CLIENT_ID,
+    NEXT_PUBLIC_MEDPLUM_PROJECT_ID: process.env.NEXT_PUBLIC_MEDPLUM_PROJECT_ID,
+    NEXT_PUBLIC_MEDPLUM_RECAPTCHA_SITE_KEY: process.env.NEXT_PUBLIC_MEDPLUM_RECAPTCHA_SITE_KEY,
+  },
 };
 
 export default nextConfig;
